0284474fcf148e9bcd7d8cbd12c8fc44a024db76
afrochef22 <70339140+afrochef22@users.noreply.github.com>
2022-06-02 15:21:54 -0700

    Card component as a wrapper

---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import "./Card.css";
+
+function Card(props) {
+  const classes = "card " + props.className;
+  // you must create a variable with props.className if you want 
+  // to use css in you costume component
+  return <div className={classes}>{props.children}</div>;
+  // to use a component as a wrapper you must 
+  // put props.children inbetween the element tags
+}
+
+export default Card;
