73ce4875622f71867880d797f01667d960b8bbb3
afrochef22 <70339140+afrochef22@users.noreply.github.com>
2022-06-30 01:08:53 -0700

    passed state from child to parent

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
 	const [enteredTitle, setEnterdTitle] = useState("");
 	const [enteredAmount, setEnterdAmount] = useState("");
 	const [enteredDate, setEnterdDate] = useState("");
@@ -58,7 +58,7 @@ const ExpenseForm = () => {
 			date: new Date(enteredDate),
 		};
 
-		console.log(expenseData);
+		props.onSaveExpenseData(expenseData);
 		setEnterdTitle("");
 		setEnterdAmount("");
 		setEnterdDate("");
