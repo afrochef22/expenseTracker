601c1faff38a4db0d58a38c7992c14c3109268f9
afrochef22 <70339140+afrochef22@users.noreply.github.com>
2022-07-22 18:58:33 -0700

    chart bar

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpenseFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
 	const [filteredYear, setFilteredYear] = useState("2020");
@@ -22,6 +23,7 @@ const Expenses = (props) => {
 				selected={filteredYear}
 				onChangeFilter={filterChangeHandler}
 			/>
+			<ExpensesChart expense={filteredExpenses} />
 			<ExpensesList items={filteredExpenses} />
 		</Card>
 	);
