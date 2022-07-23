da9e927bbedd1611f4c8fff465e49441e793d834
afrochef22 <70339140+afrochef22@users.noreply.github.com>
2022-07-23 12:29:10 -0700

    fix bug

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -4,7 +4,6 @@ import "./ChartBar.css";
 
 const ChartBar = (props) => {
 	let barFillHeight = "0%";
-
 	if (props.maxValue > 0) {
 		barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
 	}
